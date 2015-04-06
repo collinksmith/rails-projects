645a41da3c50a0ea43a2ba51738477c6fc9214ed
Collin Smith <collink33@gmail.com>
2015-04-06 14:45:21 -0700

    Update show page of todo_list

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -3,4 +3,13 @@ class TodoList < ActiveRecord::Base
 
   validates :title, presence: true, length: { minimum: 3 }
   validates :description, presence: true, length: { minimum: 5 }
+
+  def has_completed_items?
+    todo_items.complete.size > 0
+  end
+
+  def has_incomplete_items?
+    # Calling the incomplete scope ( in the TodoItem model) and making sure there's more than one item in it
+    todo_items.incomplete.size > 0
+  end
 end
