bb2406242ffef154f7aa16c879bcbd3af3aeaca9
Collin Smith <collink33@gmail.com>
2015-07-08 11:37:26 -0700

    Add associations for enrollment, user, and course

--- AssociationsExercise-master/skeleton/app/models/course.rb
+++ AssociationsExercise-master/skeleton/app/models/course.rb
@@ -1,2 +1,9 @@
 class Course < ActiveRecord::Base
+  has_many(
+    :enrollments,
+    foreign_key: :course_id,
+    primary_key: :id,
+    class_name: "Enrollment"
+  )
+  has_many :enrolled_students, through: :enrollments, source: :students
 end
--- AssociationsExercise-master/skeleton/app/models/enrollment.rb
+++ AssociationsExercise-master/skeleton/app/models/enrollment.rb
@@ -1,2 +1,14 @@
 class Enrollment < ActiveRecord::Base
+  belongs_to(
+    :students,
+    foreign_key: :student_id,
+    primary_key: :id,
+    class_name: 'User'
+  )
+  belongs_to(
+    :courses,
+    foreign_key: :course_id,
+    primary_key: :id,
+    class_name: 'Course'
+  )
 end
